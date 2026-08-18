6cf156361e6b64658fe20b903adbcb8b85f0ee30
mjapa21 <mjapa21@freeuni.edu.ge>
2026-08-19 00:31:39 +0400

    Remove old todo comments

--- app/src/main/java/com/mjapa21/smartwallet/data/local/db/model/TransactionDbo.kt
+++ app/src/main/java/com/mjapa21/smartwallet/data/local/db/model/TransactionDbo.kt
@@ -10,5 +10,5 @@ data class TransactionDbo(
     val id: Int = 0,
     val name: String,
     val amount: Double,
-    val date: Long //todo check this
+    val date: Long
 )
--- app/src/main/java/com/mjapa21/smartwallet/domain/model/UserPreferences.kt
+++ app/src/main/java/com/mjapa21/smartwallet/domain/model/UserPreferences.kt
@@ -1,15 +1,7 @@
 package com.mjapa21.smartwallet.domain.model
 
-data class UserPreferences( //todo make similar to this data class in the data/model layer too and map in the repository
+data class UserPreferences(
     val name: String,
     val monthlyIncome: Double,
     val isOnboardingComplete: Boolean
-) {
-    companion object {
-        val EMPTY = UserPreferences(
-            name = "",
-            monthlyIncome = 0.0,
-            isOnboardingComplete = false
-        )
-    }
-}
+)
